cb6fc241c910e9a93dd9d52c5ae2db49694be9e9
Muhammad Masood <muhammadmasood673@gmail.com>
2023-10-18 11:11:10 +0500

    Changes...

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Next.js13 with Tailwind, Prisma, Postgresql and Typescript.</h1>
+     <h1>NextJS'13 with Tailwind, Prisma, Postgresql and Typescript...</h1>
     </main>
   )
 }
