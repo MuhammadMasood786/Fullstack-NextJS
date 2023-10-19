7e09e2e9851c152de0ba329020718c949e7dd646
MuhammadMasood786 <53137443+MuhammadMasood786@users.noreply.github.com>
2023-10-19 12:49:58 +0500

    added prisma with railway.app and fetch data from table.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import Link from "next/link";
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPost`);
+  if (!res.ok) {
+    console.log("Not Ok: ", res);
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getPosts();
+  console.log("Data: ", data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>NextJS'13 with Tailwind, Prisma, Postgresql and Typescript...</h1>
+    <main className="py-8 px-48">
+      <Link
+        href={"/auth"}
+        className="bg-teal-500 text-black font-medium  py-2 px-4 rounded-md"
+      >
+        NextJS'13 with Tailwind, Prisma, Postgresql and Typescript
+      </Link>
     </main>
-  )
+  );
 }
